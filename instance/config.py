f26a56690710bf5943a9972f475753d005158bee
[kizzanaome] <[naomekizzan@gmail.com]>
2018-09-27 13:53:10 -0700

    Add config file

---
+++ instance/config.py
@@ -0,0 +1,22 @@
+class BaseConfig:
+    DATABASE_URL = 'postgresql://postgres:1460@localhost:5433/fast_food_db'
+    DEBUG= True
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+class TestingConfig(BaseConfig):
+    DATABASE_URL = 'postgresql://postgres:1460@localhost:5433/testing_db'
+    DEBUG = True
+    
+
+class ProductionConfig(BaseConfig):
+    Debug =False
+
+app_config={
+    "development":DevelopmentConfig,
+    "production":ProductionConfig,
+    "testing":TestingConfig
+    
+}
