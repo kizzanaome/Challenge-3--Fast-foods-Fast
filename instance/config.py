6bad4106b8ab27ba74e58c670029f8fabbe5943a
[kizzanaome] <[naomekizzan@gmail.com]>
2018-10-01 08:31:22 -0700

    Modify config file

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,4 @@
+import os
 class BaseConfig:
     
     DATABASE_URL = 'postgresql://postgres:1460@localhost:5433/fast_food_db'
@@ -10,7 +11,10 @@ class DevelopmentConfig(BaseConfig):
     DB='fast_food_db'
 
 class TestingConfig(BaseConfig):
-    DATABASE_URL = 'postgres://postgres:1460@localhost:5433/testing_db'
+    if os.getenv('TRAVIS'):
+        DATABASE_URL='postgres://postgres@localhost/testing_db'
+    else:
+        DATABASE_URL = 'postgres://postgres:1460@localhost:5433/testing_db'
     DEBUG = False
     Testing =True
     DB = 'testing_db'
