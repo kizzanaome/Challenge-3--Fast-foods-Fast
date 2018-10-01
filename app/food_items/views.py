a345b21aa0de4e03c681ba6568faf185c1043730
[kizzanaome] <[naomekizzan@gmail.com]>
2018-10-01 03:39:26 -0700

    Add file for my routes[ Delivers #160799115]

--- app/food_items/views.py
+++ app/food_items/views.py
@@ -5,24 +5,28 @@ from flask import request
 import psycopg2
 from app.database import Database
 from flask import current_app as app
-
-user_id = 1
+from app.decorator import admin_only
+from flask_jwt_extended import jwt_required,get_jwt_identity
 
 class FoodItems(Resource):
+    @jwt_required
     def get(self):
         try:
-            food = Food(user_id,"food_name","price")   
-            fd=food.fetch_all_food_items()
-            if fd ==True:
+            fd=Food.fetch_food_name_and_price()
+            if not fd:
                 return {"msg": " There are no food_items at the momnet"}, 200 
-            return make_response(jsonify({"Food_items": fd}),200)
+            return make_response(jsonify({"Food_Menu": fd}),200)
             
         except (Exception, psycopg2.DatabaseError)as Error:
             print(Error)
    
-
+    @jwt_required
+    @admin_only
     def post(self):
         """ defining the request parser and expected arguments in the request"""
+
+        current_user = get_jwt_identity()
+
         parser= reqparse.RequestParser()
         parser.add_argument("food_name",
                             type=str,
@@ -35,12 +39,11 @@ class FoodItems(Resource):
         args = parser.parse_args()
 
         """creating an insatnce of a food_items class"""
-        food = Food(user_id,args["food_name"], args["price"])
-
+        food = Food(current_user,args["food_name"], args["price"])
         create_food=food.create_foodItems ()
         if create_food:
             return make_response(jsonify({'massege':"you have succesfully placed a food_item"}),201)
-        return {"msg": "Ordfood_item not placed succesfully"}, 400
+        return {"msg": "food_item not placed succesfully"}, 400
 
 
 
