56a45265e89221a61f57c30a0e12d4c4043ee2d8
[kizzanaome] <[naomekizzan@gmail.com]>
2018-10-02 09:05:30 -0700

    modify file

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 config_name = ""
 
 if config_name == 'production':
-     DATABASE_URL = 'postgres://cnvubaunffnlsa:2639efa048a47a8de9efa076d2912fe6a7de075896b82ef48971bf4f799830b4@ec2-54-221-225-11.compute-1.amazonaws.com:5432/d35aprkl2ds50e'
+    htk = Database('postgres://cnvubaunffnlsa:2639efa048a47a8de9efa076d2912fe6a7de075896b82ef48971bf4f799830b4@ec2-54-221-225-11.compute-1.amazonaws.com:5432/d35aprkl2ds50e')
 config_name = 'development'
 
 app = create_app(config_name)
