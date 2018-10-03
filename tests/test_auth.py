f8306943bef3674a0040647dbb45b0078167e443
[kizzanaome] <[naomekizzan@gmail.com]>
2018-10-03 06:33:49 -0700

    Add tests for the user

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -6,17 +6,15 @@ from app.orders.models import Order
 from app.database import Database
 from .test_data import *
 
-
 class BaseCase(unittest.TestCase):
-    """class holds all the unittests for the app"""
+    """class holds all the unittests for the auth endpoints"""
 
     def setUp(self):
         """
             This method is run at the begining of each test
             also initialises the client where tests will be run
 
-        """
-        
+        """        
         config_name = 'testing'
         self.app = create_app(config_name)
         self.app_context = self.app.app_context()
@@ -25,33 +23,25 @@ class BaseCase(unittest.TestCase):
         self.db.create_tables()
         self.client = self.app.test_client()
 
-
     def test_user_resgistration(self):
         response = self.client.post(
             'api/v1/auth/signup', data=json.dumps(user), content_type='application/json')
-
         self.assertEqual(response.status_code, 201)
         self.assertIn('you have succesfully signed up', str(response.data))
 
-
     def test_admin_registration(self):
-
         """method for testing an admin signup"""
         response = self.client.post(
             'api/v1/auth/asignup', data=json.dumps(user), content_type='application/json')
-
         self.assertEqual(response.status_code, 201)
         self.assertIn('you have succesfully signed up', str(response.data))
 
     def test_user_login(self):
-
         """method for testing user_login endpoint"""
         response = self.client.post(
             'api/v1/auth/signup', data=json.dumps(user), content_type='application/json')
-
         response = self.client.post(
             'api/v1/auth/login', data=json.dumps(user), content_type='application/json')
-
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data.decode())
         self.assertTrue(data['token'])
