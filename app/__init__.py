1ff77216d183904b9246e9a10ca946b309ed4524
kizzanaome <naomekizzan@gmail.com>
2018-10-17 12:04:37 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, jsonify
 from instance.config import app_config
 from flask_restplus import Api
 import os
+from flask_cors import CORS
+
 
 def create_app(config_name):
     app = Flask(__name__,instance_relative_config=True)
+    CORS(app)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     env = os.getenv('FLASK_ENV')
